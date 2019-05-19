ad356fe4310f6a180f9d4bb53ecf9c4f639fa8c0
Rodrigo de Lima Oliveira <rodrigolima82@gmail.com>
2019-05-19 11:40:49 -0300

    Delete 06-ScoreModel.R

--- 06-ScoreModel.R
+++
@@ -1,9 +0,0 @@
-# Previsões com um modelo de classificação baseado em randomForest
-
-# Gerando previsões nos dados de teste
-previsoes <- data.frame(observado = dados_teste$is_attributed,
-                        previsto = predict(modeloRF, newdata = dados_teste))
-
-
-# Visualizando o resultado
-View(previsoes)
